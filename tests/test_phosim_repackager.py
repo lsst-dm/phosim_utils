96a594eb9250ec040b27c49c479e4069587dbfc6
suberlak <sci.chris@gmail.com>
2021-02-26 01:17:10 +0000

    Update e-image filename
    Add tests for amp files
    
    Add new amp and eimage test files
    
    Close eimage and amp  test file

--- tests/test_phosim_repackager.py
+++ tests/test_phosim_repackager.py
@@ -24,6 +24,7 @@ import os
 import shutil
 import astropy.io.fits as fits
 import unittest
+import glob
 
 from lsst.utils import getPackageDir
 from lsst.phosim.utils.phosim_repackager import PhoSimRepackager
@@ -42,14 +43,19 @@ class TestPhoSimRepackager(unittest.TestCase):
         self.tmp_test_dir = os.path.join(test_dir, "tmp")
         self._make_dir(self.tmp_test_dir)
 
-        self.test_data_dir = os.path.join(test_dir, "testData")
-        self.base_eimg_file_name = "lsst_e_9006001_f1_R22_S00_E000.fits"
+        self.test_data_dir_eimg = os.path.join(test_dir, "testData", "eimage")
+        self.test_data_dir_amp = os.path.join(test_dir, "testData", "amp")
+        self.base_eimg_file_name = "lsst_e_9006001_f1_R22_S22_E000.fits"
+        self.repackaged_eimg_file_name = "MC_H_20000217_006001_R22_S22_e.fits"
+        self.repackaged_amp_file_name = "MC_H_20000217_006001_R22_S22.fits"
         self.eimg_file_path = os.path.join(
-            self.test_data_dir, "%s.gz" % self.base_eimg_file_name)
+            self.test_data_dir_eimg, "%s.gz" % self.base_eimg_file_name)
+        self.amp_file_paths = sorted(glob.glob(os.path.join(self.test_data_dir_amp,
+                                     'lsst_a_*')))
 
     def _make_dir(self, directory):
 
-        if (not os.path.exists(directory)):
+        if not os.path.exists(directory):
             os.makedirs(directory)
 
     def tearDown(self):
@@ -62,7 +68,8 @@ class TestPhoSimRepackager(unittest.TestCase):
         self.assertEqual(num_file, 0)
 
         self.phoSim_repackager.process_visit_eimage(
-            self.test_data_dir, out_dir=self.tmp_test_dir, prefix='lsst')
+            self.test_data_dir_eimg, out_dir=self.tmp_test_dir, prefix="lsst"
+        )
 
         num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
         self.assertEqual(num_file, 1)
@@ -73,42 +80,98 @@ class TestPhoSimRepackager(unittest.TestCase):
         self.assertEqual(num_file, 0)
 
         self.phoSim_repackager.repackage_eimage(
-            self.eimg_file_path, out_dir=self.tmp_test_dir)
+            self.eimg_file_path, out_dir=self.tmp_test_dir
+        )
 
         num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
         self.assertEqual(num_file, 1)
 
-        file_name = self.base_eimg_file_name
+        file_name = self.repackaged_eimg_file_name
         self._check_repackaged_eimg_file(file_name)
 
     def _check_repackaged_eimg_file(self, file_name):
 
         eimg_file = os.path.join(self.tmp_test_dir, file_name)
-        sensor = fits.open(eimg_file)[0]
+        hdul = fits.open(eimg_file)
+        sensor = hdul[0]
 
         # Check the image is transposed
-        self.assertEqual(sensor.data.shape, (4000, 4072))
+        self.assertEqual(sensor.data.shape, (4004, 4096))
 
         # Check the header imformation
         header = sensor.header
-        self.assertEqual(header['RAFTNAME'], 'R22')
-        self.assertEqual(header['SENSNAME'], 'S00')
-        self.assertEqual(header['ROTANGLE'], 0.0)
+        self.assertEqual(header["RAFTBAY"], "R22")
+        self.assertEqual(header["CCDSLOT"], "S22")
+        self.assertEqual(header["RA"], 0.0)
+
+        # Close the FITS file
+        hdul.close()
+
+    def test_process_visit_amp_image(self):
+
+        num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
+        self.assertEqual(num_file, 0)
+
+        self.phoSim_repackager.process_visit(
+            self.test_data_dir_amp, out_dir=self.tmp_test_dir, prefix="lsst"
+        )
+
+        num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
+        self.assertEqual(num_file, 1)
+
+    def test_repackage_amp_image(self):
+
+        num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
+        self.assertEqual(num_file, 0)
+
+        self.phoSim_repackager.repackage(
+            self.amp_file_paths, out_dir=self.tmp_test_dir
+        )
+
+        num_file = self._get_num_of_file_in_dir(self.tmp_test_dir)
+        self.assertEqual(num_file, 1)
+
+        file_name = self.repackaged_amp_file_name
+        self._check_repackaged_amp_file(file_name)
+
+    def _check_repackaged_amp_file(self, file_name):
+
+        amp_file = os.path.join(self.tmp_test_dir, file_name)
+        hdul = fits.open(amp_file)
+
+        # Check the amp has correct shape
+        self.assertEqual(hdul[1].data.shape, (2048, 576))
+
+        # Check the number of amps: main header
+        # plus 16 amps
+        self.assertEqual(len(hdul), 17)
+
+        # Check the main header imformation
+        header = hdul[0].header
+        self.assertEqual(header["RAFTBAY"], "R22")
+        self.assertEqual(header["CCDSLOT"], "S22")
+        self.assertEqual(header["LSST_NUM"], "E2V-CCD250-370")
+        self.assertEqual(header["OBSID"], "MC_H_20000217_006001")
+        self.assertEqual(header["RA"], 0.0)
+
+        # Check the amp header information
+        header = hdul[1].header
+        self.assertEqual(header["AMPID"], "C10")
+        self.assertEqual(header["CCDID"], "R22_S22")
+        self.assertEqual(header["EXTNAME"], "Segment10")
+
+        # Close the file
+        hdul.close()
 
     def _get_num_of_file_in_dir(self, directory):
 
-        return len([name for name in os.listdir(directory)
-                   if os.path.isfile(os.path.join(directory, name))])
-
-    def test_mef_filename_eimage(self):
-
-        out_dir = os.path.join(os.sep, "path", "of", "output")
-
-        outfile = PhoSimRepackager.mef_filename_eimage(
-            self.eimg_file_path, out_dir=out_dir)
-
-        ans_outfile = os.path.join(out_dir, self.base_eimg_file_name)
-        self.assertEqual(outfile, ans_outfile)
+        return len(
+            [
+                name
+                for name in os.listdir(directory)
+                if os.path.isfile(os.path.join(directory, name))
+            ]
+        )
 
 
 if __name__ == "__main__":
